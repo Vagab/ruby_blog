6c27c87cda47b5fc3a9710eb35a6596192b1c5da
Vagab <bilyksergeaa@gmail.com>
2019-09-27 18:58:55 +0200

    link model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_153138) do
+ActiveRecord::Schema.define(version: 2019_09_27_164815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,24 @@ ActiveRecord::Schema.define(version: 2019_09_12_153138) do
     t.bigint "user_id"
     t.boolean "published", default: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "articles_tags", force: :cascade do |t|
+    t.bigint "article_id"
+    t.bigint "tag_id"
+    t.index ["article_id"], name: "index_articles_tags_on_article_id"
+    t.index ["tag_id"], name: "index_articles_tags_on_tag_id"
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.string "title"
+    t.text "url"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
