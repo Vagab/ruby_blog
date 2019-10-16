ddf0710d9f99f0c829f0e7a5fc199d737c95c6e2
Vagab <bilyksergeaa@gmail.com>
2019-10-16 20:06:35 +0200

    solid

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,6 @@ class Article < ApplicationRecord
   has_and_belongs_to_many :tags
 
   # attribute :comma_separated_tags, :string
-  validates :title, :description, presence: true
 
   # def comma_separated_tags
   #   self.tags.pluck(:name)*','
